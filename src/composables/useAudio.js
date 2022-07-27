05c352376732a6be24513926e3e9941598e82b8d
jkjh8 <jkjh82@naver.com>
2022-07-27 15:02:24 +0900

    audioMeter

--- src/composables/useAudio.js
+++ src/composables/useAudio.js
@@ -38,8 +38,8 @@ function setAudioMeter() {
     spliter.connect(analyserL, 0)
     spliter.connect(analyserR, 1)
 
-    meterDataL = new Float32Array(analyserL.fftSize)
-    meterDataR = new Float32Array(analyserR.fftSize)
+    meterDataL = new Uint8Array(analyserL.fftSize)
+    meterDataR = new Uint8Array(analyserR.fftSize)
 
     ctxL = meterL.value.getContext('2d')
     ctxR = meterR.value.getContext('2d')
@@ -57,27 +57,39 @@ function drawMeter() {
     ctxR.clearRect(0, 0, meterR.value.width, meterR.value.height)
     levelL.value = 0
     levelR.value = 0
-    analyserL.getFloatTimeDomainData(meterDataL)
-    analyserL.getFloatTimeDomainData(meterDataR)
+    analyserL.getByteFrequencyData(meterDataL)
+    analyserR.getByteFrequencyData(meterDataR)
 
     // console.log(Math.max(meterDataL))
 
     for (let i = 0; i < meterDataL.length; i++) {
-      const powerL = meterDataL[i] ** 2
-      levelL.value = Math.max(powerL, meterDataL[i])
-
-      const powerR = meterDataR[i] ** 2
-      levelR.value = Math.max(powerR, meterDataR[i])
+      let powerL, powerR
+      if (meterDataL[i] !== 0) {
+        if (meterDataL[i] > 128) {
+          powerL = meterDataL[i] % 128
+        } else {
+          powerL = 128 - meterDataL[i]
+        }
+        levelL.value = Math.max(levelL.value, powerL)
+      }
+      if (meterDataR[i] !== 0) {
+        if (meterDataR[i] > 128) {
+          powerR = meterDataR[i] % 128
+        } else {
+          powerR = 128 - meterDataR[i]
+        }
+        levelR.value = Math.max(levelR.value, powerR)
+      }
     }
-
-    if (10 * Math.log10(levelL.value) > -2) {
+    console.log(levelL.value)
+    if (levelL.value > 125) {
       if (peakIntervalL) {
         clearTimeout(peakIntervalL)
       }
       setPeak('L')
     }
 
-    if (10 * Math.log10(levelR.value) > -2) {
+    if (levelR.value > 125) {
       if (peakIntervalR) {
         clearTimeout(peakIntervalR)
       }
@@ -86,36 +98,19 @@ function drawMeter() {
 
     smoothLevelL = 0.9 * smoothLevelL + 0.1 * levelL.value
     smoothLevelR = 0.9 * smoothLevelR + 0.1 * levelR.value
-    levelDbL.value = 10 * Math.log10(smoothLevelL)
-    levelDbR.value = 10 * Math.log10(smoothLevelR)
-
-    if (smoothLevelL > -3) {
-      ctxL.fillStyle = '#1976d2'
-    } else if (smoothLevelL <= -3 && smoothLevelL > -6) {
-      ctxL.fillStyle = 'yellow'
-    } else {
-      ctxL.fillStyle = '#1976d2'
-    }
-    if (smoothLevelR > -3) {
-      ctxR.fillStyle = '#1976d2'
-    } else if (smoothLevelR <= -3 && smoothLevelR > -6) {
-      ctxR.fillStyle = 'yellow'
-    } else {
-      ctxR.fillStyle = '#1976d2'
-    }
+    ctxL.fillStyle = '#1976d2'
+    ctxR.fillStyle = '#1976d2'
 
     ctxL.fillRect(
       0,
       0,
-      meterL.value.width +
-        (meterL.value.width / 100) * levelDbL.value,
+      (meterL.value.width / 128) * smoothLevelL,
       meterL.value.height
     )
     ctxR.fillRect(
       0,
       0,
-      meterR.value.width +
-        (meterR.value.width / 100) * levelDbR.value,
+      (meterR.value.width / 128) * smoothLevelR,
       meterR.value.height
     )
   } catch (err) {
